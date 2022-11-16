a3149491262729b8abf980e2c7ccd7f0b34ebaee
MAZzzeen <104923042+MAZzzeen@users.noreply.github.com>
2022-11-17 00:03:55 +0200

    Add files via upload

--- config.fish
+++ config.fish
@@ -1,13 +1,20 @@
-if status is-interactive
-    pgrep bspwm || startx
+if status --is-login
+  if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
+    exec startx
+  end
 end
 
-clear
-neofetch
+starship init fish | source
+
+pfetch
 
 alias yeet='sudo pacman -Rns'
 alias get='sudo pacman -S'
 alias update='sudo pacman -Syu'
-alias cls='clear && neofetch'
+alias cls='clear && pfetch'
 alias :q='exit'
-alias image='kitty +kitten icat '
+alias :Q='exit'
+alias image='kitty +kitten icat'
+alias reboot='sudo reboot'
+alias ls='exa -la'
+alias sleep='sudo loginctl suspend'
