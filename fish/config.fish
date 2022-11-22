4feacb2fa3566056ad4ef8d974682522ed783a34
MAZzzeen <104923042+MAZzzeen@users.noreply.github.com>
2022-11-22 16:10:33 +0200

    Add files via upload

--- fish/config.fish
+++ fish/config.fish
@@ -6,15 +6,12 @@ end
 
 starship init fish | source
 
-pfetch
+macchina
 
 alias yeet='doas pacman -Rns'
 alias get='doas pacman -S'
 alias update='doas pacman -Syu'
-alias cls='clear && pfetch'
+alias cls='clear && macchina'
 alias :q='exit'
 alias :Q='exit'
-alias image='kitty +kitten icat'
-alias reboot='doas reboot'
 alias ls='exa -la'
-alias sleep='doas loginctl suspend'
